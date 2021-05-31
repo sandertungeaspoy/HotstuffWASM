b6ce8e605d79ec83cfd3a7faccc1cf8be91985b6
sandertungeaspoy <35257578+sandertungeaspoy@users.noreply.github.com>
2021-05-31 09:32:47 +0200

    Speedup
    And Cmd debug

--- newNetwork/main/main.go
+++ newNetwork/main/main.go
@@ -685,11 +685,11 @@ func ConnectToPeer() (*webrtc.DataChannel, string) {
 			break
 		}
 
-		if attempts > 10 {
+		if attempts > 50 {
 			return nil, "error"
 		}
 		attempts++
-		time.Sleep(time.Millisecond * 5000)
+		time.Sleep(time.Millisecond * 500)
 	}
 
 	offersdp := webrtc.SessionDescription{Type: webrtc.SDPTypeOffer, SDP: offer}
@@ -886,11 +886,11 @@ func ConnectToLeader() (*webrtc.DataChannel, string) {
 		if answer != "empty" {
 			break
 		}
-		if attempts > 10 {
+		if attempts > 50 {
 			return nil, "error"
 		}
 		attempts++
-		time.Sleep(time.Millisecond * 5000)
+		time.Sleep(time.Millisecond * 500)
 	}
 
 	// fmt.Println(answer)
